9ec99d36ac4056f29cd75406fa542f797d0f4144
lulopes2001 <lopeslucia2001@gmail.com>
2025-01-29 03:28:22 +0000

    ll

--- client/src/Components/Updates.jsx
+++ client/src/Components/Updates.jsx
@@ -2,7 +2,12 @@ import "../Styles/Updates.css";
 export function Updates() {
   return (
     <div className="updates-container">
-      <h1>Updates</h1>
+      <h1>Atualizações</h1>
+      <input type="text" 
+           id="readOnlyTextBox" 
+           readonly 
+           value="Ultima Atualização">
+      </input>
     </div>
   );
 }
